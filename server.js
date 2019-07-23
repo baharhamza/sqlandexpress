fc8f4b0e4533cafbb5f6a11f0cdeec86fec12133
Student <jsitbon@qcc.cuny.edu>
2019-07-23 17:51:22 -0400

    Added the working project

--- server.js
+++ server.js
@@ -2,6 +2,24 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const port = 4000;
+const mysql = require('mysql');
+
+
+const mysqlConnection = mysql.createConnection({
+  host: "10.9.3.218",
+  user: "TWStudent",
+  password: "TechWorks!",
+  database: "employeedb",
+  multipleStatements: true
+});
+
+mysqlConnection.connect(err =>{
+  if(!err) console.log("DB connection succeded")
+  else{
+    console.log("DB connection failed Error: " + JSON.stringify(err, undefined, 2))
+  }
+
+})
 
 //Middleware
 app.use(morgan('dev'));
@@ -12,6 +30,24 @@ app.listen(port, () => {
   console.log(`Server Started on Port ${port}`);
 });
 
+//routes 
+app.get("/employees", (req, res) =>{
+  mysqlConnection.query("SELECT * FROM Employee", (err, rows, field) =>{
+    if(!err) res.send(rows); 
+    else console.log(err);
+  })
+});
+
+app.get("/employees/:id", (req,res) =>{
+  mysqlConnection.query(
+    "SELECT * FROM Employee WHERE EmpID =?",
+    [req.params.id],
+    (err, rows, field) =>{
+      if(!err) res.send(rows);
+      else console.log(err);
+    }
+  );
+});
 app.get('/api/students', (req, res) => {
   const students = [
     { id: 1, firstName: 'Captain', lastName: 'fancy' },
